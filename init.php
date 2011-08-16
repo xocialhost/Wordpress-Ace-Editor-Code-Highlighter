41b73e3efa52c340e5f76a5f824abe73bbfa1476
XocialHost <xocialhost@personal-fx.com>
2011-08-16 17:29:40 -0600

    initial file upload

---
+++ init.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Ace WP Code Highlighter
+Plugin URI: https://github.com/xocialhost/Wordpress-Ace-Editor-Code-Highlighter
+Description: Integration of Ace Editor with Wordpress for code highlighting
+Version: 1.0 //Version of your plugin
+Author: Dustin Nielson
+Author URI: http://www.xocialhost.com/ //Url of your site
+License: MIT
+*/
